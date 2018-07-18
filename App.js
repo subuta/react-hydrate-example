f9fd84359822f2d0ee5b21e4b0070c005162bac7
subuta <shunta.3110@gmail.com>
2018-07-18 21:44:54 +0900

    Add hydrate.

--- App.js
+++ App.js
@@ -1,8 +1,16 @@
 import React from 'react'
 import { hot } from 'react-hot-loader'
 
-const App = () => (
-  <h1>aaaa</h1>
-)
+const App = () => {
+  const onClick = () => {
+    console.log('clicked!')
+  }
+
+  return (
+    <button onClick={onClick}>
+      click me!
+    </button>
+  )
+}
 
 export default hot(module)(App)
--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 import ReactDOM from 'react-dom'
 import App from './App'
+import { html } from 'common-tags'
+
+const body = document.querySelector('body')
+
+body.innerHTML = html`
+    <div id="app"><button>click me!</button></div>
+`
 
 const node = document.querySelector('#app')
 
-ReactDOM.render(<App />, node)
+ReactDOM.hydrate(<App />, node)
